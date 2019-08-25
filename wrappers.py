11aac2b7bde38081bb21647d43295eaaa324a960
LeulShiferaw <abiyleoul3@gmail.com>
2019-08-25 19:22:16 +0200

    Initial repo

---
+++ wrappers.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class DTPytorchWrapper():
+    def __init__(self, shape=(120, 160, 3)):
+        self.shape = shape
+        self.transposed_shape = (shape[2], shape[0], shape[1])
+
+    def preprocess(self, obs):
+        from PIL import Image
+        return np.array(Image.fromarray(observation).resize(self.shape)).transpose(2, 0, 1)
